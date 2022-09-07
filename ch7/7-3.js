35ce40af7c696bc3a95a766f964e0a3d1af1a97d
Jeon Yerim <talentforest0501@gmail.com>
2022-09-07 17:03:54 +0900

    Refactoring: 7-3 기본형을 객체로 바꾸기

--- ch7/7-3.js
+++ ch7/7-3.js
@@ -2,14 +2,20 @@ export class Order {
   constructor(data) {
     this.priority = data.priority;
   }
+
+  isHighPrioriry() {
+    return "high" === this.priority || "rush" === this.priority;
+  }
 }
 
 const orders = [
-  new Order({ priority: 'normal' }),
-  new Order({ priority: 'high' }),
-  new Order({ priority: 'rush' }),
+  new Order({ priority: "normal" }),
+  new Order({ priority: "high" }),
+  new Order({ priority: "rush" }),
 ];
 
-const highPriorityCount = orders.filter(
-  (o) => 'high' === o.priority || 'rush' === o.priority
-).length;
+const highPriorityCount = orders.filter((o) => o.isHighPrioriry()).length;
+
+console.log(highPriorityCount);
+
+// 프로젝트 전반적으로 Priority를 많이 사용한다면 따로 클래스를 만들 수 있다.
