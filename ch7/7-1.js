c684843e4a79537ee63a5bcc853cdd83a589524b
Jeon Yerim <talentforest0501@gmail.com>
2022-09-07 16:24:55 +0900

    Refactoring: 7-1 레코드 캡슐화하기

--- ch7/7-1.js
+++ ch7/7-1.js
@@ -1,5 +1,43 @@
-const organization = { name: 'Acme Gooseberries', country: 'GB' };
+class Organization {
+  #name;
+  #country;
+  constructor(name, country) {
+    this.#name = name;
+    this.#country = country;
+  }
 
-organization.name = 'Dream Coding';
+  get name() {
+    return this.#name;
+  }
+
+  set name(value) {
+    this.#name = value;
+  }
+
+  get country() {
+    return this.#country;
+  }
+
+  set country(value) {
+    this.#country = value;
+  }
+
+  get rawData() {
+    return { name: this.name, country: this.country };
+  }
+}
+
+const organization = new Organization("Acme Gooseberries", "GB");
+
 console.log(organization.name);
 console.log(organization.country);
+
+organization.name = "ellie";
+organization.country = "US";
+
+console.log(organization.name);
+console.log(organization.country);
+
+// 레코드 캡슐화하기
+// Object.freeze
+// Class
