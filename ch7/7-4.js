0cca3833c979fb949b2d859f6ab683095b21b364
Jeon Yerim <talentforest0501@gmail.com>
2022-09-07 17:11:34 +0900

    Refactoring: 7-4 임시 변수를 질의 함수로 바꾸기

--- ch7/7-4.js
+++ ch7/7-4.js
@@ -6,10 +6,18 @@ class Order {
     this.#item = item;
   }
 
+  get basePrice() {
+    return this.#quantity * this.#item.price;
+  }
+
+  get discountFactor() {
+    return this.basePrice > 1000 ? 0.95 : 0.98;
+  }
+
   get price() {
-    const basePrice = this.#quantity * this.#item.price;
-    const discountFactor = 0.98;
-    if (basePrice > 1000) discountFactor -= 0.03;
-    return basePrice * discountFactor;
+    return this.basePrice * this.discountFactor;
   }
 }
+
+const a = new Order(3, { item: "coffee", price: 3000 });
+console.log(a.price);
