855868fa631336eeddeb5c58c9eb7055482347dd
jellie <talentforest0501@gmail.com>
2022-09-12 20:05:38 +0900

    Refactoring: 11-1 질의 함수와 변경 함수 분리하기

--- ch11/11-1.js
+++ ch11/11-1.js
@@ -1,28 +1,33 @@
 // 예제 1
-function totalOutstandingAndSendBill() {
+function totalOutstanding() {
   const result = customer.invoices.reduce(
     (total, each) => each.amount + total,
     0
   );
-  sendBill();
-  return result;
+}
+
+function sendBill() {
+  // Bill 보내는 코드
 }
 
 // 예제 2
 export function alertForMiscreant(people, alarm) {
+  const miscreant = findMiscreant(people);
+  setOffAlarms(alarm, miscreant);
+}
+
+function findMiscreant() {
   for (const p of people) {
-    if (p === 'Don') {
-      setOffAlarms(alarm, p);
-      return 'Don';
+    if (p === "Don") {
+      return "Don";
     }
-    if (p === 'John') {
-      setOffAlarms(alarm, p);
-      return 'John';
+    if (p === "John") {
+      return "John";
     }
   }
-  return '';
+  return "";
 }
 
 function setOffAlarms(alarm, p) {
-  alarm.setOff('Found Miscreant ' + p);
+  alarm.setOff("Found Miscreant " + p);
 }
